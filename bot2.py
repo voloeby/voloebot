dae2d84dbcfe8363e63746f7e57f91a21905cde0
Nestor P <hectopk@gmail.com>
2020-02-02 23:09:12 +0300

    edit message

--- bot2.py
+++ bot2.py
@@ -1,70 +1,5 @@
-
-TOKEN = '1086036256:AAEhfZzvm3huQhNM7asL7O1DXUtZsRBocx8'
-
-# from telegram.ext import Updater
-# import token
-#
-# updater = Updater(token=TOKEN, use_context=True)
-
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-"""Simple Bot to reply to Telegram messages.
-This is built on the API wrapper, see echobot2.py to see the same example built
-on the telegram.ext bot framework.
-This program is dedicated to the public domain under the CC0 license.
-"""
-# import logging
-# import telegram
-# from telegram.error import NetworkError, Unauthorized
-# from time import sleep
-#
-#
-# update_id = None
-#
-#
-# def main():
-#     """Run the bot."""
-#     global update_id
-#     # Telegram Bot Authorization Token
-#     bot = telegram.Bot(TOKEN)
-#
-#     # get the first pending update_id, this is so we can skip over it in case
-#     # we get an "Unauthorized" exception.
-#     try:
-#         update_id = bot.get_updates()[0].update_id
-#     except IndexError:
-#         update_id = None
-#
-#     logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-#
-#     while True:
-#         try:
-#             echo(bot)
-#         except NetworkError:
-#             sleep(1)
-#         except Unauthorized:
-#             # The user has removed or blocked the bot.
-#             update_id += 1
-#
-#
-# def echo(bot):
-#     """Echo the message the user sent."""
-#     global update_id
-#     # Request updates after the last update_id
-#     for update in bot.get_updates(offset=update_id, timeout=10):
-#         update_id = update.update_id + 1
-#
-#         if update.message:  # your bot can receive updates without messages
-#             # Reply to the message
-#             update.message.reply_text(update.message.text)
-
-# from telegram.ext import Updater, CommandHandler
-# import logging
 import telegramtoken
 import telebot
-from telebot import apihelper
-
-apihelper.proxy = {'http':'https:/115.160.21.243:8000'}
 
 TOKEN = telegramtoken.TOKEN
 
@@ -84,7 +19,7 @@ def echo_all(message):
 				bot.send_message(message.chat.id, 'Каких ' + str + '?')
 
 	else:
-		bot.send_message(message.chat.id, 'ты пидор(ка)')
+		bot.send_message(message.chat.id, 'ты пидор')
 
 
 bot.polling()
