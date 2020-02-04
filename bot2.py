1a034c0d3ae57826293f3e471ed83e276665085e
Nestor P <hectopk@gmail.com>
2020-02-04 21:19:17 +0300

    bug fix

--- bot2.py
+++ bot2.py
@@ -9,17 +9,16 @@ bot = telebot.TeleBot(TOKEN)
 def echo_all(message):
 	print(message.chat.type)
 	if message.chat.type == 'group':
-		if message.text[-2:] == 'во':
-			bot.send_message(message.chat.id, message.text + 'лоеб')
+		# if message.text[-2:] == 'во':
+		# 	bot.send_message(message.chat.id, message.text + 'лоеб')
 		if message.text.lower() == 'нет':
 			bot.send_message(message.chat.id, 'пидора ответ')
 		strs = message.text.split(' ')
 		for str in strs:
-			if str[-2:] == 'ов':
+			if str[-2:].lower() == 'ов':
 				bot.send_message(message.chat.id, 'Каких ' + str + '?')
-			if message.text[-2:] == 'во':
+			if message.text[-2:].lower() == 'во':
 				bot.send_message(message.chat.id, str + 'лоеб')
-
 	else:
 		bot.send_message(message.chat.id, 'ты пидор')
 
