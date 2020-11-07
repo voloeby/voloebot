66b9a996184741c7c401cd9bd0e525de6e4384ae
Nestor P <hectopk@gmail.com>
2020-11-07 18:39:55 +0300

    update logging

--- internal/mysqlrepo/logging.py
+++ internal/mysqlrepo/logging.py
@@ -14,8 +14,7 @@ class LogsRepo:
 		self.user = user
 		self.password = password
 		self.db = db
-	def save_log(self, data):
-		connection = pymysql.connect(
+		self.conn =  pymysql.connect(
 			host=self.host,
 			port=int(self.port),
 			user=self.user,
@@ -24,17 +23,27 @@ class LogsRepo:
 			charset="utf8",
 			use_unicode=True,
 		)
+	def save_log(self, data):
+		# connection = pymysql.connect(
+		# 	host=self.host,
+		# 	port=int(self.port),
+		# 	user=self.user,
+		# 	password=self.password,
+		# 	db=self.db,
+		# 	charset="utf8",
+		# 	use_unicode=True,
+		# )
 		try:
-			with connection.cursor() as cursor:
-				sql = '''INSERT INTO tg_bot_logs (`message_text`, `user_id`) VALUES (%s, %s)'''
+			with self.conn.cursor() as cursor:
+				sql = '''INSERT INTO tg_bot_logs (`message_text`, `user_id`, `name`) VALUES (%s, %s, %s)'''
 				print("sql", sql)
 				print("text", data.text)
 				print("user_id", data.from_user.id)
-				r = cursor.execute(sql, (data.text, data.from_user.id))
-				connection.commit()
+				r = cursor.execute(sql, (data.text, data.from_user.id, data.from_user.username))
+				conn.commit()
 				print(r)
 				print(str(data.__dict__))
 		except Exception as e:
 			print(e)
-		finally:
-			connection.close()
+		# finally:
+		# 	connection.close()
